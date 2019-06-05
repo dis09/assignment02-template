a84411d912b393038abf02a36c93bafd9cb6a441
bonartm <malte.bonart@th-koeln.de>
2019-06-05 08:26:22 +0200

    fix tests

--- tests/test_ex1.py
+++ tests/test_ex1.py
@@ -5,22 +5,22 @@ from pytest import approx
 
 @pytest.mark.points(5)
 def test_n_zdf():
-    assert ex1.n_zdf == 55
+    assert ex1.n_zdf == 117
 
 
 @pytest.mark.points(5)
 def test_n_rt():
-    assert ex1.n_rt == 117
+    assert ex1.n_rt == 55
 
 
 @pytest.mark.points(5)
 def test_mean_likes_zdf():
-    assert ex1.mean_likes_zdf == approx(508.3455, abs=1e-2)
+    assert ex1.mean_likes_zdf == approx(510.4615, abs=1e-2)
 
 
 @pytest.mark.points(5)
 def test_mean_likes_rt():
-    assert ex1.mean_likes_rt == approx(510.4615, abs=1e-2)
+    assert ex1.mean_likes_rt == approx(508.3455, abs=1e-2)
 
 
 @pytest.mark.points(15)
